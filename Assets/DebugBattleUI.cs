bf8e77049dcfdc53bd013bfe40b5302f70674a6a
Roman <45533413+RomanMalyshev@users.noreply.github.com>
2023-07-09 18:55:35 +0400

    hern9

---
+++ Assets/DebugBattleUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugBattleUI : MonoBehaviour
+{
+    public TMP_Text Round;
+    public TMP_Text UnitName;
+    public TMP_Text AP;
+    public Button Move;
+    public Button Attack;
+
+    private void Start()
+    {
+        
+        
+    }
+}
---
+++ Assets/Scripts/Game/Battle/Properties/Attacker.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace.Battle;
+using RedBjorn.ProtoTiles;
+using UnityEngine;
+
+public class Attacker : CommandHandler
+{
+    public override Action onHandlerEnd { get; set; }
+    
+    public override void Init(MapEntity mapEntity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Activate()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Handle()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Deactivate()
+    {
+        throw new NotImplementedException();
+    }
+}
