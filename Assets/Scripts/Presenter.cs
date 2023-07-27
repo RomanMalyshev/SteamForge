285fbd988a9f5c65b4713ba48762f03ba9955c60
Roman <noagard@gmail.com>
2023-07-27 22:36:55 +0300

    camp

--- Assets/Scripts/Presenter.cs
+++ Assets/Scripts/Presenter.cs
@@ -1,4 +1,6 @@
-﻿using Game.Battle;
+﻿using System.Collections.Generic;
+using DefaultNamespace.Player;
+using Game.Battle;
 using UnityEngine;
 using static UnityEngine.Rendering.DebugUI;
 
@@ -28,8 +30,8 @@ namespace DefaultNamespace
                 Globals.Global.Model.Plyer.Value.Moral += isHumanEnemy ? -1 : +1;
                 Globals.Global.Model.ChangedPlayerExp.Value -= 100;
                 Globals.Global.Model.ChangedPlayerMoral.Value = isHumanEnemy ? -1 : +1;
-                
-                
+
+
                 foreach (var character in party)
                 {
                     character.Exp += 100;
@@ -44,15 +46,78 @@ namespace DefaultNamespace
 
                 Globals.Global.Model.Plyer.Invoke(Globals.Global.Model.Plyer.Value);
             });
-            
+
             Globals.Global.Model.Plyer.Subscribe(player =>
             {
                 if (player != null)
                 {
                     var playerJson = JsonUtility.ToJson(player);
-                    PlayerPrefs.SetString("player",playerJson);
+                    PlayerPrefs.SetString("player", playerJson);
                 }
-            } );
+            });
+
+            if (PlayerPrefs.HasKey("player"))
+            {
+                Globals.Global.Model.Plyer.Value = JsonUtility.FromJson<Player.Player>(PlayerPrefs.GetString("player"));
+            }
+            else
+            {
+                Globals.Global.Model.Plyer.Value = new Player.Player()
+                {
+                    Currency = 1000,
+                    Gears = 10000,
+                    Party = new List<Character>()
+                    {
+                        new Character()
+                        {
+                            Name = "Tank",
+                            Level = 1,
+                            Health = { Value = 100 },
+                            MoveRange = { Value = 2 },
+                            ActionPoints = { Value = 8 },
+                            Damage = { Value = 400 },
+                            AttackRange = { Value = 2 },
+                            Initiative = { Value = 1 },
+                        },
+                        new Character()
+                        {
+                            Name = "Bug",
+                            Level = 1,
+                            Health = { Value = 90 },
+                            MoveRange = { Value = 3 },
+                            ActionPoints = { Value = 12 },
+                            Damage = { Value = 1500 },
+                            AttackRange = { Value = 1 },
+                            Initiative = { Value = 1 },
+                        },
+                        new Character()
+                        {
+                            Name = "Caster",
+                            Level = 1,
+                            Health = { Value = 150 },
+                            MoveRange = { Value = 4 },
+                            ActionPoints = { Value = 9 },
+                            Damage = { Value = 500 },
+                            AttackRange = { Value = 1 },
+                            Initiative = { Value = 0 },
+                        },
+                        new Character()
+                        {
+                            Name = "SteamMachine",
+                            Level = 1,
+                            Health = { Value = 75 },
+                            MoveRange = { Value = 3 },
+                            ActionPoints = { Value = 18 },
+                            Damage =
+                            {
+                                Value = 2500
+                            },
+                            AttackRange = { Value = 1 },
+                            Initiative = { Value = 1 },
+                        },
+                    }
+                };
+            }
         }
     }
 }
