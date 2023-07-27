0a37d9fb1261c1ecedbe09405a0392daadcc2d2b
Skvirel1 <Skvirel2008@gmail.com>
2023-07-27 23:15:45 +0300

    OnMapUI added

---
+++ Assets/OnMapUI.cs
@@ -0,0 +1,53 @@
+using DefaultNamespace.Player;
+using DefaultNamespace;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+
+public class OnMapUI : MonoBehaviour
+{
+    [SerializeField] private List<TMP_Text> _names;
+    [SerializeField] private List<TMP_Text> _levels;
+    [SerializeField] private List<TMP_Text> _exps;
+    [SerializeField] private Slider _moralMarker;
+
+    private Player _player;
+    private View _view;    
+
+    private void Start()
+    {
+        _view = Globals.Global.View;
+
+        Globals.Global.Model.Plyer.Subscribe(player =>
+        {
+            _player = player;
+            SetupInfo();
+        }); 
+
+        if (Globals.Global.Model.Plyer.Value != null && Globals.Global.Model.Plyer.Value.Party.Count > 0)
+        {
+            _player = Globals.Global.Model.Plyer.Value;
+            SetupInfo();
+        }
+    }
+
+    private void SetupInfo()
+    {
+        for (var i = 0; i < _player.Party.Count; i++)
+        {
+            _names[i].text = _player.Party[i].Name;
+            _levels[i].text = $"AP: {_player.Party[i].Level}";
+            _exps[i].text = $"Exp: {_player.Party[i].Exp}";            
+        }
+        Debug.Log(_player.Moral);
+
+        _moralMarker.minValue = -5;
+        _moralMarker.maxValue = 5;
+        _moralMarker.value = _player.Moral;
+    }
+
+   
+}
--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -8,20 +8,31 @@ public class UIController : MonoBehaviour
 {
     [SerializeField] private DebugBattleUI _debugBattleUI;
     [SerializeField] private PartyUpgrade _upgradeUI;
+    [SerializeField] private OnMapUI _onMapUI;
 
 
     private void Start()
     {
         _debugBattleUI.gameObject.SetActive(false);
         _upgradeUI.gameObject.SetActive(false);
-        Globals.Global.View.ActiveBattle.Subscribe(map => { _debugBattleUI.gameObject.SetActive(true); });
+
+        Globals.Global.View.ActiveBattle.Subscribe(map => 
+        {
+            _debugBattleUI.gameObject.SetActive(true);
+            _onMapUI.gameObject.SetActive(false);
+        });
 
         Globals.Global.View.ActiveBattle.Subscribe(map => { _debugBattleUI.gameObject.SetActive(true); });
 
-        Globals.Global.View.OnMapCampClick.Subscribe(() => { _upgradeUI.gameObject.SetActive(true); });
+        Globals.Global.View.OnMapCampClick.Subscribe(() =>
+        {
+            _upgradeUI.gameObject.SetActive(true);
+            _onMapUI.gameObject.SetActive(false);
+        });
 
         Globals.Global.View.ReturnToMap.Subscribe(() =>
         {
+            _onMapUI.gameObject.SetActive(true);
             _upgradeUI.gameObject.SetActive(false);
             _debugBattleUI.gameObject.SetActive(false);
         });
